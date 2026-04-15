3cae414826f15319313cf93e12c4d8a6f79baacf
Rinriku69 <siripoohkim@gmail.com>
2026-04-15 14:38:34 +0700

    feat: MultipleChoices quiz layout

---
+++ frontend/app/quiz/QuizContainer.tsx
@@ -0,0 +1,70 @@
+"use client";
+import { MultipleChoicesQuiz } from "../types/types";
+import { useState } from "react";
+export default function QuizContainer({ quizes }: { quizes: MultipleChoicesQuiz[] }) {
+    const [currentIndex, setCurrentIndex] = useState(0);
+    const [userAnswers, setUserAnswers] = useState<Record<number, number>>({}); 
+
+    const currentQuiz = quizes[currentIndex];
+
+    const handleSelect = (choiceId: number) => {
+        setUserAnswers({ ...userAnswers, [currentQuiz.questionNumber]: choiceId });
+        
+      
+        if (currentIndex < quizes.length - 1) {
+            setCurrentIndex(currentIndex + 1); 
+        }
+    };
+
+    return (
+        <div className="flex flex-col p-4 border-4 border-red-300 ">
+            <h2 className="text-xl font-bold align-center">
+                Qustion {currentQuiz.questionNumber}: {currentQuiz.questionText}
+            </h2>
+
+            <div className=" mt-4 border-3 border-blue-300 grid grid-cols-2">
+                {currentQuiz.choices.map((choice) => (
+                    <button
+                        key={choice.id}
+                        onClick={() => handleSelect(choice.id)}
+                        className={`p-2 border rounded ${
+                            userAnswers[currentQuiz.questionNumber] === choice.id 
+                            ? "bg-blue-500 text-white" 
+                            : "bg-white"
+                        } text-black`}
+                    >
+                        {choice.character} ({choice.romaji})
+                    </button>
+                ))}
+            </div>
+
+            <div className="mt-6 flex justify-between">
+                <button 
+                    disabled={currentIndex === 0}
+                    onClick={() => setCurrentIndex(currentIndex - 1)}
+                    className="px-4 py-2 bg-gray-200 rounded disabled:opacity-50 text-black"
+                >
+                    Back
+                </button>
+                
+                <span>{currentIndex + 1} / {quizes.length}</span>
+
+                {currentIndex < quizes.length - 1 ? (
+                    <button 
+                        onClick={() => setCurrentIndex(currentIndex + 1)}
+                        className="px-4 py-2 bg-gray-200 rounded text-black"
+                    >
+                        Next
+                    </button>
+                ) : (
+                    <button 
+                        onClick={() => console.log("ส่งคำตอบ:", userAnswers)}
+                        className="px-4 py-2 bg-green-500 text-white rounded"
+                    >
+                        Submit
+                    </button>
+                )}
+            </div>
+        </div>
+    );
+}
--- frontend/app/quiz/page.tsx
+++ frontend/app/quiz/page.tsx
@@ -1,37 +1,19 @@
+import { MultipleChoicesQuiz } from "../types/types"
+import QuizContainer from "./QuizContainer";
 
-interface Quiz {
-    questionId:string;
-    questionNumber:number;
-    questionText:string;
-    choices: Choice[];
-}
 
-interface Choice{
-    id:number;
-    character:string;
-    romaji:string;
-    type:string;
-}
-
-async function getQuiz(): Promise<Quiz[]>{
+async function getQuiz(): Promise<MultipleChoicesQuiz[]>{
     const res = await fetch('http://localhost:8080/getquiz')
     return res.json()
 }
 
 
 export default async function Quiz(){
-    const quizes = await getQuiz()
+    const quizes: MultipleChoicesQuiz[] = await getQuiz()
     return (
-        <div>
-            {quizes.map(q=>(
-                <div key={q.questionNumber}>
-                    <div>{q.questionNumber}</div>
-                    <div>{q.questionText}</div>
-                    {q.choices.map(c=>(
-                        <div key={c.id}>{c.romaji}</div>
-                    ))}
-                </div>
-            ))}
-        </div>
-    )
+        <main>
+            <h1 className="text-center">Japanese Language Quiz</h1>
+            <QuizContainer quizes={quizes} />
+        </main>
+    );
 }
